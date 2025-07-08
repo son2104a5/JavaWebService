ae0a4af178d9cd99cb88fa7c3761b1ee3d99f597
son21804a5 <sonnguyen21804@gmail.com>
2025-07-08 13:28:47 +0700

    cac

---
+++ LT/ss02/src/main/java/com/data/Ss02Application.java
@@ -0,0 +1,13 @@
+package com.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss02Application.class, args);
+    }
+
+}
