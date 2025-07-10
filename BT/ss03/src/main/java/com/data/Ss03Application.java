1aa81e2e923e86efe4a908bdfb5c0fa7d65714c2
son21804a5 <sonnguyen21804@gmail.com>
2025-07-10 13:24:27 +0700

    111

---
+++ BT/ss03/src/main/java/com/data/Ss03Application.java
@@ -0,0 +1,13 @@
+package com.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss03Application.class, args);
+    }
+
+}
