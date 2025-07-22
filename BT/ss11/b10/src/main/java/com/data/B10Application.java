cdeaba8fbe85408fef3a4e061510d679ed81d1b8
son21804a5 <sonnguyen21804@gmail.com>
2025-07-22 20:02:12 +0700

    1

---
+++ BT/ss11/b10/src/main/java/com/data/B10Application.java
@@ -0,0 +1,13 @@
+package com.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B10Application.class, args);
+    }
+
+}
