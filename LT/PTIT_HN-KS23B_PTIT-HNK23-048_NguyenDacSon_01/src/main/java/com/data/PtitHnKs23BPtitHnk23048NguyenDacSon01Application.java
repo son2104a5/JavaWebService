9cf57fde9b1def9f0dc5eb9187bdfcc371bb9553
son21804a5 <sonnguyen21804@gmail.com>
2025-07-23 13:56:17 +0700

    1

---
+++ LT/PTIT_HN-KS23B_PTIT-HNK23-048_NguyenDacSon_01/src/main/java/com/data/PtitHnKs23BPtitHnk23048NguyenDacSon01Application.java
@@ -0,0 +1,13 @@
+package com.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtitHnKs23BPtitHnk23048NguyenDacSon01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtitHnKs23BPtitHnk23048NguyenDacSon01Application.class, args);
+    }
+
+}
---
+++ LT/PTIT_HN-KS23B_PTIT-HNK23-048_NguyenDacSon_01/src/test/java/com/data/PtitHnKs23BPtitHnk23048NguyenDacSon01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtitHnKs23BPtitHnk23048NguyenDacSon01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
