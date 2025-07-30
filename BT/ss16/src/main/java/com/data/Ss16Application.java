b06f201320aaec12db81dcb5e71b2aababc02c93
son21804a5 <sonnguyen21804@gmail.com>
2025-07-31 00:45:43 +0700

    1

--- BT/ss16/src/main/java/com/data/Ss16Application.java
+++ BT/ss16/src/main/java/com/data/Ss16Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestFinalApplication {
+public class Ss16Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestFinalApplication.class, args);
+        SpringApplication.run(Ss16Application.class, args);
     }
 
 }
